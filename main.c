601d77ddcda2e4d4961d1beaaf5977cb568a2bac
mgabrielaa1988 <mariagabrielaaragon@hotmail.com>
2018-03-19 12:25:54 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int base;
+    int altura;
+    printf("Ingrese la base: %d", base);
+    scanf("%d",&base);
+    printf("Ingrese la altura: %d", altura);
+    fflush(stdin);
+    scanf("%d", &altura);
+
+    return 0;
+}
